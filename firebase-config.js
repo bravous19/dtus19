27ee8c17617576cfaec1554a522817f6d5b1cc88
bravous19 <kdevilcoder@gmail.com>
2026-05-10 02:36:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3h2KkvK3-bFIETWoGFV5MwzKCF3bp6KM",
+  authDomain: "private-chat-app-d0af5.firebaseapp.com",
+  projectId: "private-chat-app-d0af5",
+  storageBucket: "private-chat-app-d0af5.appspot.com",
+  messagingSenderId: "1070638135082",
+  appId: "1:1070638135082:web:a91683af4224c13d9f5053"
+};
+
+export const VAPID_KEY = "5mOE8tvzrV_7oWhVXANOak58oq7SAxm7y5hREGcSLXM";
